39673e36180e26dfc27394347afc5872376ee5f3
Jack Waugh <git.yipzdu@spamgourmet.com>
2013-12-23 03:49:48 +0000

    Correction.

--- src/left/petition_ext_link.rb
+++ src/left/petition_ext_link.rb
@@ -1,7 +1,6 @@
 div :class => "article_summary" do
     a "Petition", :href =>
-      "http://www.facebook.com/l.php?u=http%3A%2F%2Fchange.org%2Fpetitions%2F" +
-      "bill-of-rights-for-voting-equality-voters-fourth-branch-of-government" +
-      "&h=kAQH0Prdp"
+      "http://www.change.org/petitions/" +
+      "bill-of-rights-for-voting-equality-voters-fourth-branch-of-government"
 end
 
